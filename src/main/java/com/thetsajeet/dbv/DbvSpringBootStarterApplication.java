9cac87c13ec4cdd36be85020efb72d2162e7ac93
T S Ajeet <ajeet.tumuluru@gmail.com>
2025-10-16 13:47:14 +0530

    setup from spring initializr

---
+++ src/main/java/com/thetsajeet/dbv/DbvSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.thetsajeet.dbv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbvSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbvSpringBootStarterApplication.class, args);
+	}
+
+}
