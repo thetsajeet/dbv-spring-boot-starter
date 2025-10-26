ad9aeaa8c10048688c95e222aed7bbcaded65a44
T S Ajeet <66743734+thetsajeet@users.noreply.github.com>
2025-10-26 13:39:23 +0530

    validation logic for sequences #6 (#22) (#23)

--- src/main/java/com/thetsajeet/dbv/DatabaseValidationService.java
+++ src/main/java/com/thetsajeet/dbv/DatabaseValidationService.java
@@ -2,6 +2,7 @@ package com.thetsajeet.dbv;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.metamodel.EntityType;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -43,6 +45,19 @@ class DatabaseValidationService {
                     log.info("Table {} exists in the database.", tableName);
 
                     validateColumns(entity, tableName, connection, metaData);
+
+                    String sequenceName = getSequenceNameFromEntity(javaType);
+
+                    if (sequenceName == null || sequenceName.isEmpty()) {
+                        log.info("No sequence found on {}. Skipping sequence validator...", javaType);
+                        continue;
+                    }
+
+                    if (checkSequenceExists(connection, sequenceName)) {
+                        log.info("Sequence {} exists for entity {}", sequenceName, javaType.getSimpleName());
+                    } else {
+                        log.error("Missing sequence {} for entity {}", sequenceName, javaType.getSimpleName());
+                    }
                 }
             }
         }
@@ -65,6 +80,20 @@ class DatabaseValidationService {
         return entityManagerFactory.getMetamodel().entity(entityClass).getName();
     }
 
+    private String getSequenceNameFromEntity(Class<?> entityClass) {
+        // Check for @SequenceGenerator annotation
+        if (entityClass.isAnnotationPresent(SequenceGenerator.class)) {
+            SequenceGenerator sequenceGenerator = entityClass.getAnnotation(SequenceGenerator.class);
+            if(!sequenceGenerator.sequenceName().isEmpty()) {
+                return sequenceGenerator.sequenceName();
+            }
+            if (!sequenceGenerator.name().isEmpty()) {
+                return sequenceGenerator.name();
+            }
+        }
+        return null;
+    }
+
     private void validateColumns(EntityType<?> entity, String tableName, Connection conn, DatabaseMetaData metaData) throws SQLException {
         log.info("Scanning all columns in {}", entity.getName());
 
@@ -83,6 +112,34 @@ class DatabaseValidationService {
 
                 log.info("Column found: {}", columnName);
             }
+        }
+    }
+
+    private String getSequenceCheckQuery(String dbProduct) {
+        if (dbProduct.contains("postgresql") || dbProduct.contains("h2") || dbProduct.contains("mysql")) {
+            return "SELECT sequence_name FROM information_schema.sequences WHERE sequence_name = ?";
+        } else if (dbProduct.contains("oracle")) {
+            return "SELECT sequence_name FROM all_sequences WHERE sequence_name = ?";
+        } else if (dbProduct.contains("sql server")) {
+            return "SELECT name AS sequence_name FROM sys.sequences WHERE name = ?";
+        } else {
+            throw new UnsupportedOperationException("Unknown DB dialect for sequence validation: " + dbProduct);
+        }
+    }
+
+    private boolean checkSequenceExists(Connection connection, String sequenceName) {
+        try {
+            String dbProduct = connection.getMetaData().getDatabaseProductName().toLowerCase();
+            String sql = getSequenceCheckQuery(dbProduct);
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setString(1, sequenceName);
+                try (ResultSet rs = ps.executeQuery()) {
+                    return rs.next();
+                }
+            }
+        } catch (SQLException e) {
+            log.error("Error checking sequence '{}': {}", sequenceName, e.getMessage());
+            return false;
         }
     }
 }
--- src/test/java/com/thetsajeet/dbv/DatabaseValidationServiceTest.java
+++ src/test/java/com/thetsajeet/dbv/DatabaseValidationServiceTest.java
@@ -1,8 +1,15 @@
 package com.thetsajeet.dbv;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @ExtendWith(MockitoExtension.class)
 class DatabaseValidationServiceTest {
+    @Autowired
+    private DatabaseValidationService databaseValidationService;
+
+    @Test
+    void testGetTableNameFromEntity() {}
 }
